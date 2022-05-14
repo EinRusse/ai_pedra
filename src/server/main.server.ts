cb270377d53efbd97ccfd3f8e95dafd989e41b12
Raposa <pietro.ra2007@gmail.com>
2022-05-14 02:16:48 -0300

    Commit before changing OS

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -17,24 +17,37 @@ RunService.Heartbeat.Connect(() => {
 			};
 			const tween = TweenService.Create(ai, ti, position);
 			tween.Play();
+			tween.Completed.Connect(() => {
+				if (object.front !== undefined) {
+					const left = ai.WaitForChild("LeftEye") as Beam;
+					const right = ai.WaitForChild("RightEye") as Beam;
+
+					left.Enabled = true;
+					right.Enabled = true;
+					object.front.Instance.Destroy();
+					wait(0.5);
+					left.Enabled = false;
+					right.Enabled = false;
+				}
+			});
 		} else if (object.right !== undefined) {
 			print("hito direita");
 			const rotation = {
-				Rotation: ai.Rotation.add(new Vector3(0, 25, 0)),
+				CFrame: ai.CFrame.mul(CFrame.Angles(0, math.rad(-30), 0)),
 			};
 			const tween = TweenService.Create(ai, ti, rotation);
 			tween.Play();
 		} else if (object.left !== undefined) {
 			print("hito esquerda");
 			const rotation = {
-				Rotation: ai.Rotation.sub(new Vector3(0, 25, 0)),
+				CFrame: ai.CFrame.mul(CFrame.Angles(0, math.rad(30), 0)),
 			};
 			const tween = TweenService.Create(ai, ti, rotation);
 			tween.Play();
 		} else {
 			print("hito ninguem");
 			const rotation = {
-				Rotation: ai.Rotation.sub(new Vector3(0, -25 * math.random(-1, 1), 0)),
+				CFrame: ai.CFrame.mul(CFrame.Angles(0, math.rad(30 * math.random(-1, 1)), 0)),
 			};
 			const tween = TweenService.Create(ai, ti, rotation);
 			tween.Play();
@@ -44,9 +57,39 @@ RunService.Heartbeat.Connect(() => {
 	}
 });
 
+function rightCast() {
+	const upright = Workspace.Raycast(ai.Position, ai.CFrame.VectorToWorldSpace(new Vector3(5, 0, -10)));
+	if (upright !== undefined) {
+		return upright;
+	}
+	const right = Workspace.Raycast(ai.Position, ai.CFrame.VectorToWorldSpace(new Vector3(10, 0, -10)));
+	if (right !== undefined) {
+		return right;
+	}
+	const downright = Workspace.Raycast(ai.Position, ai.CFrame.VectorToWorldSpace(new Vector3(15, 0, -10)));
+	if (downright !== undefined) {
+		return downright;
+	}
+}
+
+function leftCast() {
+	const upleft = Workspace.Raycast(ai.Position, ai.CFrame.VectorToWorldSpace(new Vector3(-5, 0, -10)));
+	if (upleft !== undefined) {
+		return upleft;
+	}
+	const left = Workspace.Raycast(ai.Position, ai.CFrame.VectorToWorldSpace(new Vector3(-10, 0, -10)));
+	if (left !== undefined) {
+		return left;
+	}
+	const downleft = Workspace.Raycast(ai.Position, ai.CFrame.VectorToWorldSpace(new Vector3(-15, 0, -10)));
+	if (downleft !== undefined) {
+		return downleft;
+	}
+}
+
 function createRaycast() {
 	const front = Workspace.Raycast(ai.Position, ai.CFrame.LookVector.mul(10));
-	const right = Workspace.Raycast(ai.Position, ai.CFrame.VectorToWorldSpace(new Vector3(10, 0, -1)));
-	const left = Workspace.Raycast(ai.Position, ai.CFrame.VectorToWorldSpace(new Vector3(-10, 0, -1)));
+	const right = rightCast();
+	const left = leftCast();
 	return { front, right, left };
 }
